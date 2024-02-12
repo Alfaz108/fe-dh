32fd709b33890f6e6570adf498ca12a6ff0f5422
maruf bellah <mdmaruf5511507@gmail.com>
2024-02-13 01:52:58 +0600

    sidebar menu desing and dinamic way impliment

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { RouterProvider } from "react-router-dom";
 import { route } from "./routes/index";
 import "bootstrap/dist/css/bootstrap.min.css";
+// import MenuBar from "./pages/nav/MenuBar";
+
+import MenuBar from "/src/pages/nav/MenuBar"; // Correct import
 
 function App() {
   return (
     <>
-      <RouterProvider router={route} />
+      {/* <RouterProvider router={route} /> */}
+      <MenuBar></MenuBar>
     </>
   );
 }
